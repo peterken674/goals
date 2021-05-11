570531ce22886f2d0c55e32ea3c20ea0a28af1c8
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-05-12 01:13:21 +0300

    Emittting events.

--- src/app/goal-detail/goal-detail.component.ts
+++ src/app/goal-detail/goal-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Goal } from '../goal';
 
 @Component({
@@ -9,6 +9,11 @@ import { Goal } from '../goal';
 export class GoalDetailComponent implements OnInit {
 
     @Input() goal: Goal;
+    @Output() isComplete = new EventEmitter<boolean>();
+
+    goalComplete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
   constructor() { }
 
   ngOnInit(): void {
